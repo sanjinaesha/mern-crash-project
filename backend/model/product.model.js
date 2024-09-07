6d0bf966ce837ee45745da5ad72755106174a131
sanjinaesha <76872022+sanjinaesha@users.noreply.github.com>
2024-09-07 06:12:08 +0000

    next

---
+++ backend/model/product.model.js
@@ -0,0 +1,20 @@
+import mongoose,{Schema,model} from "mongoose";
+
+const productSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+})
+
+export const Product = model('Product',productSchema)
